587b0616682e3bac64fe9496220223ebe62f3cd1
Christian Stewart <kidovate@gmail.com>
2014-03-14 07:48:49 -0700

    Added 'close' method

--- D2MPClient/D2MP.cs
+++ D2MPClient/D2MP.cs
@@ -3,9 +3,12 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Net;
 using System.Reflection;
 using System.Text.RegularExpressions;
 using System.Threading;
+using ICSharpCode.SharpZipLib.Core;
+using ICSharpCode.SharpZipLib.Zip;
 using Microsoft.Win32;
 using WebSocketSharp;
 
@@ -25,6 +28,37 @@ namespace d2mp
             info.RedirectStandardOutput = true;
             info.UseShellExecute = false;
             Process.Start(info);
+        }
+
+        static void UnzipFromStream(Stream zipStream, string outFolder)
+        {
+
+            ZipInputStream zipInputStream = new ZipInputStream(zipStream);
+            ZipEntry zipEntry = zipInputStream.GetNextEntry();
+            while (zipEntry != null)
+            {
+                String entryFileName = zipEntry.Name;
+                // to remove the folder from the entry:- entryFileName = Path.GetFileName(entryFileName);
+                // Optionally match entrynames against a selection list here to skip as desired.
+                // The unpacked length is available in the zipEntry.Size property.
+
+                byte[] buffer = new byte[4096];     // 4K is optimum
+
+                // Manipulate the output filename here as desired.
+                String fullZipToPath = Path.Combine(outFolder, entryFileName);
+                string directoryName = Path.GetDirectoryName(fullZipToPath);
+                if (directoryName.Length > 0)
+                    Directory.CreateDirectory(directoryName);
+
+                // Unzip file in buffered chunks. This is just as fast as unpacking to a buffer the full size
+                // of the file, but does not waste memory.
+                // The "using" will close the stream even if an exception occurs.
+                using (FileStream streamWriter = File.Create(fullZipToPath))
+                {
+                    StreamUtils.Copy(zipInputStream, streamWriter, buffer);
+                }
+                zipEntry = zipInputStream.GetNextEntry();
+            }
         }
 
         static void UninstallD2MP()
@@ -61,12 +95,32 @@ namespace d2mp
                     log.Debug("Steam found: " + steamDir);
                 }
 
+                var addonsDir = Path.Combine(steamDir, "steamapps/common/dota 2 beta/dota/addons/");
+                if (!Directory.Exists(addonsDir))
+                {
+                    log.Fatal("Addons dir: " + addonsDir + " does not exist.");
+                    return;
+                }
+
+                string[] modNames = null;
+                {
+                    var dirs = Directory.GetDirectories(addonsDir);
+                    modNames = new string[dirs.Length];
+                    int i = 0;
+                    foreach (var dir in dirs)
+                    {
+                        modNames[i] = Path.GetFileName(dir);
+                        i++;
+                    }
+                }
+
                 //Detect user
                 var config = File.ReadAllText(Path.Combine(steamDir, @"config\config.vdf"));
                 var matches = Regex.Matches(config, "\"\\d{17}\"");
                 string steamid;
                 List<string> steamids = new List<string>();
-                if(matches.Count > 0){
+                if (matches.Count > 0)
+                {
                     foreach (Match match in matches)
                     {
                         steamid = match.Value.Substring(1).Substring(0, match.Value.Length - 2);
@@ -82,7 +136,8 @@ namespace d2mp
 
                 bool shutDown = false;
                 int tryCount = 0;
-                while(tryCount < 10 && !shutDown){
+                while (tryCount < 10 && !shutDown)
+                {
                     using (var ws = new WebSocket(server))
                     {
                         ws.OnMessage += (sender, e) =>
@@ -94,7 +149,15 @@ namespace d2mp
                                     shutDown = true;
                                     return;
                                 }
-                                else if (e.Data == "uninstall")
+
+                                if (e.Data == "close")
+                                {
+                                    log.Debug("Shutting down due to server request.");
+                                    shutDown = true;
+                                    return;
+                                }
+
+                                if (e.Data == "uninstall")
                                 {
                                     log.Debug("Uninstalling due to server request...");
                                     UninstallD2MP();
@@ -108,7 +171,18 @@ namespace d2mp
                                 {
                                     case "installmod":
                                         var modname = msgParts[1];
-                                        log.Info("Server requested that we install mod " + modname);
+                                        log.Info("Server requested that we install mod " + modname + " from download " + msgParts[2]);
+                                        //delete if already exists
+                                        var targetDir = Path.Combine(addonsDir, modname);
+                                        if (Directory.Exists(targetDir))
+                                            Directory.Delete(targetDir, true);
+                                        //Make the dir again
+                                        Directory.CreateDirectory(targetDir);
+                                        //Stream the ZIP to the folder
+                                        WebClient client = new WebClient();
+                                        UnzipFromStream(client.OpenRead(msgParts[2]), targetDir);
+                                        log.Info("Mod installed!");
+                                        ws.Send("installedMod:"+modname);
                                         break;
                                     default:
                                         log.Error("Command not recognized: " + msgParts[0]);
@@ -134,7 +208,7 @@ namespace d2mp
                                     Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location),
                                                  "version.txt"));
                             log.Debug("sending version: " + ver);
-                            ws.Send("init:" +String.Join(",", steamids.ToArray(), 0, steamids.Count) + ":" + ver);
+                            ws.Send("init:" + String.Join(",", steamids.ToArray(), 0, steamids.Count) + ":" + ver + ":" + String.Join(",",modNames));
                         }
                         catch (Exception ex)
                         {
@@ -143,14 +217,15 @@ namespace d2mp
                         }
 
                         tryCount = 0;
-                        while(ws.IsAlive && !shutDown)
+                        while (ws.IsAlive && !shutDown)
                         {
                             Thread.Sleep(100);
                         }
                     }
                     Thread.Sleep(1000);
                 }
-            }catch(Exception ex)
+            }
+            catch (Exception ex)
             {
                 log.Fatal("Overall error in the program: "+ex);
             }
