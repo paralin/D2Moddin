5cc462e33b8b7fb055f52d753f3bb3d46df7d7b1
Christian Stewart <kidovate@gmail.com>
2014-04-10 16:17:03 -0700

    Pushed version

--- d2mpserver/ServerUpdater.cs
+++ d2mpserver/ServerUpdater.cs
@@ -12,7 +12,7 @@ namespace d2mpserver
 {
     public class ServerUpdater
     {
-        public static string version = "1.0.2";
+        public static string version = "1.0.3";
         private static string fromUrl;
 
         public static void UpdateFromURL(string url)
