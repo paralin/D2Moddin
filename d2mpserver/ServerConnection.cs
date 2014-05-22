413c6c9681e33deadab8ea94ec951958bb0bd497
Christian Stewart <kidovate@gmail.com>
2014-05-22 15:05:29 -0700

    Updated ServerConnection download process

--- d2mpserver/ServerConnection.cs
+++ d2mpserver/ServerConnection.cs
@@ -9,261 +9,267 @@ using d2mpserver.Properties;
 
 namespace d2mpserver
 {
-    public class ServerConnection
+public class ServerConnection
+{
+    private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+    public WebSocket socket = null;
+    public bool infoValid = true;
+    private bool shutdown = false;
+    private bool reconnecting = false;
+    private ServerManager manager;
+
+    public ServerConnection(ServerManager manager)
     {
-        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
-        public WebSocket socket = null;
-        public bool infoValid = true;
-        private bool shutdown = false;
-        private bool reconnecting = false;
-        private ServerManager manager;
+        this.manager = manager;
+    }
 
-        public ServerConnection(ServerManager manager)
+    void RegisterCallbacks()
+    {
+        socket.OnClose += (sender, args) =>
         {
-            this.manager = manager;
-        }
-
-        void RegisterCallbacks()
+            log.Debug("Disconnected from the server");
+            reconnecting = true;
+        };
+        socket.OnError += (sender, args) =>
         {
-            socket.OnClose += (sender, args) =>
-                                    {
-                                        log.Debug("Disconnected from the server");
-                                        reconnecting = true;
-                                    };
-            socket.OnError += (sender, args) =>
-                                    {
-                                        log.Error("Socket error: "+args.Message);
-                                    };
-            socket.OnMessage += (sender, args) =>
-                                    {
-                                        log.Debug("Server message: "+args.Data);
-                                        ProcessMessage(args.Data);
-                                    };
-            socket.OnOpen += (sender, args) =>
-                                    {
-                                        log.Info("Connected to the server.");
-                                        SendInit();
-                                    };
-        }
-
-        private void PerformAddonOps(object state)
+            log.Error("Socket error: "+args.Message);
+        };
+        socket.OnMessage += (sender, args) =>
         {
-            string[] command = (string[]) state;
+            log.Debug("Server message: "+args.Data);
+            ProcessMessage(args.Data);
+        };
+        socket.OnOpen += (sender, args) =>
+        {
+            log.Info("Connected to the server.");
+            SendInit();
+        };
+    }
 
-            if (command[1] != "")
+    private void PerformAddonOps(object state)
+    {
+        string[] command = (string[]) state;
+
+        if (command[1] != "")
+        {
+            log.Debug("Installing addons: " + command[1]);
+            string[] addons = command[1].Split(',');
+            foreach (var addon in addons)
             {
-                log.Debug("Installing addons: " + command[1]);
-                string[] addons = command[1].Split(',');
-                foreach (var addon in addons)
-                {
-                    InstallAddon(addon);
-                }
+                InstallAddon(addon);
             }
-            if (command[2] != "")
+        }
+        if (command[2] != "")
+        {
+            string[] deletions = command[2].Split(',');
+            log.Debug("Deleting addons: " + command[2]);
+            foreach (var deletion in deletions)
             {
-                string[] deletions = command[2].Split(',');
-                log.Debug("Deleting addons: " + command[2]);
-                foreach (var deletion in deletions)
-                {
-                    DeleteAddon(deletion);
-                }
+                DeleteAddon(deletion);
             }
+        }
+        SendInit();
+    }
+
+    private void ProcessMessage(string data)
+    {
+        string[] command = data.Split('|');
+        switch(command[0])
+        {
+        case "shutdown":
+        {
+            Program.ShutdownAll();
+        }
+        break;
+        case "restart":
+        {
+            ServerUpdater.RestartD2MP();
+            Program.ShutdownAll();
+        }
+        break;
+        case "reinit":
+        {
             SendInit();
         }
-
-        private void ProcessMessage(string data)
+        break;
+        case "addonOps":
         {
-            string[] command = data.Split('|');
-            switch(command[0])
-            {
-                case "shutdown":
-                    {
-                        Program.ShutdownAll();
-                    }
-                    break;
-                case "restart":
-                    {
-                        ServerUpdater.RestartD2MP();
-                        Program.ShutdownAll();
-                    }
-                    break;
-                case "reinit":
-                    {
-                      SendInit();
-                    }
-                    break;
-                case "addonOps":
-                    {
-                        ThreadPool.QueueUserWorkItem(PerformAddonOps, command);
-                    }
-                    break;
-                case "launchServer":
-                    {
-                        int id = int.Parse(command[1]);
-                        int port = int.Parse(command[2]);
-                        bool dev = bool.Parse(command[3]);
-                        string mod = command[4];
-                        string rconPass = command[5];
-                        var serv = manager.LaunchServer(id, port, dev, mod, rconPass);
-                        serv.OnReady += (sender, args) =>
-                            {
-                                socket.SendAsync("serverLaunched|"+id, b=>{});
-                                log.Debug("server finished launching "+id);
-                            };
-                        serv.OnShutdown += (sender, args) =>
-                            {
-                                socket.SendAsync("onShutdown|" + id,
-                                                b => { });
-                                log.Debug("server shut down on its own: "+id);
-                            };
-                        break;
-                    }
-                case "shutdownServer":
-                    {
-                        int id = int.Parse(command[1]);
-                        manager.ShutdownServer(id);
-                    }
-                    break;
-                case "authFail":
-                    log.Debug("Auth password: " + Settings.Default.connectPassword + " is invalid.");
-                    log.Fatal("Server doesn't like our init info (we're probably out of date), shutting down...");
-                    infoValid = false;
-                    Environment.Exit(0);
-                    break;
-                case "outOfDate":
-                    log.Info("Server is out of date (current version is "+ServerUpdater.version+"), updating...");
-                    if (!Settings.Default.disableUpdate)
-                    {
-                        var url = command[1];
-                        log.Debug("Downloading update from " + url + "...");
-                        ServerUpdater.UpdateFromURL(url);
-                        break;
-                    }
-                    else
-                    {
-                        log.Fatal("Server out of date but auto updating disabled. Exiting...");
-                        Program.shutdown = true;
-                    }
-                    break;
-            }
+            ThreadPool.QueueUserWorkItem(PerformAddonOps, command);
         }
-
-        private void InstallAddon(string addon)
+        break;
+        case "launchServer":
         {
-            log.Debug("Attempting to install "+addon);
-            var parts = addon.Split('=');
-            //get path to addon
-            var path = Path.Combine(ServerManager.addonsPath, parts[0]);
-            if (Directory.Exists(path))
+            int id = int.Parse(command[1]);
+            int port = int.Parse(command[2]);
+            bool dev = bool.Parse(command[3]);
+            string mod = command[4];
+            string rconPass = command[5];
+            var serv = manager.LaunchServer(id, port, dev, mod, rconPass);
+            serv.OnReady += (sender, args) =>
             {
-                Directory.Delete(path, true);
-            }
-            Directory.CreateDirectory(path);
-            //download
-            using (var client = new WebClient())
+                socket.SendAsync("serverLaunched|"+id, b=> {});
+                log.Debug("server finished launching "+id);
+            };
+            serv.OnShutdown += (sender, args) =>
             {
-                Utils.UnzipFromStream(client.OpenRead(parts[2].Replace('+', '=')), ServerManager.addonsPath);
-            }
-            log.Debug("Addon downloaded and unzipped: "+parts[0]);
-            var mapspath = Path.Combine(path, "maps");
-            if (Directory.Exists(mapspath))
-            {
-                log.Debug("Maps directory exists, copying maps...");
-                var gmapspath = Path.Combine(ServerManager.gameRoot, "dota/maps/");
-                foreach (var file in Directory.GetFiles(mapspath))
-                    File.Copy(file, Path.Combine(gmapspath, Path.GetFileName(file)), true);
-            }
+                socket.SendAsync("onShutdown|" + id,
+                b => { });
+                log.Debug("server shut down on its own: "+id);
+            };
+            break;
         }
-
-        private void DeleteAddon(string addon)
+        case "shutdownServer":
         {
-            log.Debug("Attempting to delete " + addon);
-            //get path to addon
-            var path = Path.Combine(ServerManager.addonsPath, addon);
-            if (Directory.Exists(path))
+            int id = int.Parse(command[1]);
+            manager.ShutdownServer(id);
+        }
+        break;
+        case "authFail":
+            log.Debug("Auth password: " + Settings.Default.connectPassword + " is invalid.");
+            log.Fatal("Server doesn't like our init info (we're probably out of date), shutting down...");
+            infoValid = false;
+            Environment.Exit(0);
+            break;
+        case "outOfDate":
+            log.Info("Server is out of date (current version is "+ServerUpdater.version+"), updating...");
+            if (!Settings.Default.disableUpdate)
             {
-                Directory.Delete(path, true);
+                var url = command[1];
+                log.Debug("Downloading update from " + url + "...");
+                ServerUpdater.UpdateFromURL(url);
+                break;
             }
             else
             {
-                log.Error("Addon doesn't exist?");
+                log.Fatal("Server out of date but auto updating disabled. Exiting...");
+                Program.shutdown = true;
             }
-        }
-
-        public void SendInit()
-        {
-            var msg = "init|" + Settings.Default.connectPassword + "|" + Settings.Default.serverCount + "|" + GetAddonVersionsString()+"|"+ServerUpdater.version+"|"+Settings.Default.portRangeStart+"-"+Settings.Default.portRangeEnd;
-            socket.Send(msg);
-        }
-
-        private string GetAddonVersionsString()
-        {
-            return manager.GetAddonVersions();
-        }
-
-        public bool Connect()
-        {
-            if(socket != null && socket.IsAlive)
-            {
-                log.Debug("Already connected.");
-                return true;
-            }
-            log.Debug("attempting connection to "+Settings.Default.serverIP);
-            socket = new WebSocket(Settings.Default.serverIP);
-            RegisterCallbacks();
-            try
-            {
-                socket.Connect();
-            }catch(Exception ex)
-            {
-                log.Error("Problem connecting: "+ex);
-                return false;
-            }
-
-            return socket.IsAlive;
-        }
-
-        public void Disconnect()
-        {
-            if (socket == null || !socket.IsAlive)
-                return;
-
-            socket.Close();
-            socket = null;
-        }
-
-        public void Shutdown()
-        {
-            shutdown = true;
-            log.Debug("Shutting down...");
-        }
-
-        public void StartServerThread()
-        {
-            ThreadPool.QueueUserWorkItem(ServerThread);
-        }
-
-        private void ServerThread(object state)
-        {
-            while(!shutdown)
-            {
-                if (reconnecting)
-                {
-                    log.Debug("Attempting reconnection...");
-                    if (!Connect())
-                    {
-                        log.Debug("... failed, will try again in 10 seconds ...");
-                        manager.ShutdownAllServers();
-                        Thread.Sleep(9900);
-                    }
-                    else
-                    {
-                        reconnecting = false;
-                    }
-                }
-                Thread.Sleep(100);
-            }
-            manager.ShutdownAllServers();
+            break;
         }
     }
+
+    private void InstallAddon(string addon)
+    {
+        log.Debug("Attempting to install "+addon);
+        var parts = addon.Split('=');
+        //get path to addon
+        var path = Path.Combine(ServerManager.addonsPath, parts[0]);
+        if (Directory.Exists(path))
+        {
+            Directory.Delete(path, true);
+        }
+        Directory.CreateDirectory(path);
+        using (var client = new WebClient())
+        {
+            using(var mem = new MemoryStream(client.DownloadData(parts[2].Replace('+', '=')))) {
+                mem.Position = 0;
+                try{
+                  Utils.UnzipFromStream(mem, ServerManager.addonsPath);
+                }catch(Exception ex){
+                  log.Error("Failed to unzip the mod properly!");
+                }
+            }
+        }
+        log.Debug("Addon downloaded and unzipped: "+parts[0]);
+        var mapspath = Path.Combine(path, "maps");
+        if (Directory.Exists(mapspath))
+        {
+            log.Debug("Maps directory exists, copying maps...");
+            var gmapspath = Path.Combine(ServerManager.gameRoot, "dota/maps/");
+            foreach (var file in Directory.GetFiles(mapspath))
+                File.Copy(file, Path.Combine(gmapspath, Path.GetFileName(file)), true);
+        }
+    }
+
+    private void DeleteAddon(string addon)
+    {
+        log.Debug("Attempting to delete " + addon);
+        //get path to addon
+        var path = Path.Combine(ServerManager.addonsPath, addon);
+        if (Directory.Exists(path))
+        {
+            Directory.Delete(path, true);
+        }
+        else
+        {
+            log.Error("Addon doesn't exist?");
+        }
+    }
+
+    public void SendInit()
+    {
+        var msg = "init|" + Settings.Default.connectPassword + "|" + Settings.Default.serverCount + "|" + GetAddonVersionsString()+"|"+ServerUpdater.version+"|"+Settings.Default.portRangeStart+"-"+Settings.Default.portRangeEnd;
+        socket.Send(msg);
+    }
+
+    private string GetAddonVersionsString()
+    {
+        return manager.GetAddonVersions();
+    }
+
+    public bool Connect()
+    {
+        if(socket != null && socket.IsAlive)
+        {
+            log.Debug("Already connected.");
+            return true;
+        }
+        log.Debug("attempting connection to "+Settings.Default.serverIP);
+        socket = new WebSocket(Settings.Default.serverIP);
+        RegisterCallbacks();
+        try
+        {
+            socket.Connect();
+        } catch(Exception ex)
+        {
+            log.Error("Problem connecting: "+ex);
+            return false;
+        }
+
+        return socket.IsAlive;
+    }
+
+    public void Disconnect()
+    {
+        if (socket == null || !socket.IsAlive)
+            return;
+
+        socket.Close();
+        socket = null;
+    }
+
+    public void Shutdown()
+    {
+        shutdown = true;
+        log.Debug("Shutting down...");
+    }
+
+    public void StartServerThread()
+    {
+        ThreadPool.QueueUserWorkItem(ServerThread);
+    }
+
+    private void ServerThread(object state)
+    {
+        while(!shutdown)
+        {
+            if (reconnecting)
+            {
+                log.Debug("Attempting reconnection...");
+                if (!Connect())
+                {
+                    log.Debug("... failed, will try again in 10 seconds ...");
+                    manager.ShutdownAllServers();
+                    Thread.Sleep(9900);
+                }
+                else
+                {
+                    reconnecting = false;
+                }
+            }
+            Thread.Sleep(100);
+        }
+        manager.ShutdownAllServers();
+    }
+}
 }
